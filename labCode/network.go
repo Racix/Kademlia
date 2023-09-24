70cea4ddd32e9e2feda25a1fe0e6ca04cabef8f2
Claude Hallard <claudehallard@hotmail.com>
2023-09-24 13:35:09 +0200

    remove server part

--- labCode/network.go
+++ labCode/network.go
@@ -1,11 +1,8 @@
 package d7024e
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net"
-	"strconv"
 )
 
 type Network struct {
@@ -13,64 +10,7 @@ type Network struct {
 	RoutingTable *RoutingTable
 }
 
-type RPCdata struct {
-	RPCtype  string    `json:"rpcType"`
-	SenderID string    `json:"senderID"`
-	TargetID string    `json:"targetID"`
-	Value    string    `json:"value"`
-	Contacts []Contact `json:"contacts"`
-}
-
-func Listen(ip string, port int) {
-	addr, err := net.ResolveUDPAddr("udp", ip+":"+strconv.Itoa(port))
-	if err != nil {
-		log.Fatal(err)
-	}
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	buffer := make([]byte, 1024)
-	for {
-		n, addr, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			return
-		}
-		data := buffer[:n]
-		fmt.Printf("Received data from %s: %s\n", addr, data)
-
-		unMarshalledData, err := UnmarshalRPCdata(data)
-		if err != nil {
-			return
-		}
-		go HandlerRPC(unMarshalledData)
-	}
-
-}
-
-func HandlerRPC(RPC *RPCdata) {
-
-	switch RPC.RPCtype {
-	case "PING":
-		// PING
-	case "STORE":
-		// STOR
-
-	case "FIND_VALUE":
-		//FIND_VALUE
-
-	case "FIND_NODE":
-		//FIND_NODE
-
-	default:
-		// defualt
-	}
-	return
-}
-
-func (network *Network) SendPingMessage(contact *Contact) {
+func (network *Network) SendPingMessage(targetAddress string, rpc *RPCdata) {
 	// TODO
 }
 
@@ -86,24 +26,15 @@ func (network *Network) SendStoreMessage(data []byte) {
 	// TODO
 }
 
-func MarshalRPCdata() {
-
-}
-
-func UnmarshalRPCdata(data []byte) (*RPCdata, error) {
-	var rpcData RPCdata
-	err := json.Unmarshal(data, &rpcData)
-	if err != nil {
-		return nil, err
-	}
-	return &rpcData, nil
-}
-
 func (network *Network) Talk(contact *Contact) {
-	conn, err := net.DialUDP("udp", contact)
+	conn, err := net.DialUDP("udp", *contact.Address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
+}
+
+func MarshalRPCdata() {
 
 }
