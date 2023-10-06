04406a27d291a32f85001b69692fef81874a844f
Racix <abboshon2000@gmail.com>
2023-10-06 02:23:56 +0000

    update main

--- main.go
+++ main.go
@@ -3,10 +3,41 @@ package main
 import (
 	"kademlia-project/labCode"
 	"os"
+	"log"
 
 )
 
+const (
+	hostIp = "10.0.1.2:8080"
+)
+
 func main() {
-	var kad d7024e.Kademlia
+	var net d7024e.Network
+	id := d7024e.NewKademliaID("9f786a0eef5a4b9e0f7dc37212344491e64ccce8")
+	me := d7024e.NewContact(id, hostIp)
+	ip, err := d7024e.GetLocalIPAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if ip == hostIp {
+		rt := d7024e.NewRoutingTable(me)
+		/*rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+		rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+		rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
+		rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8002"))
+		rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8002"))
+		rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("3111111400000000000000000000000000000000"), "localhost:8002"))*/	
+		net = *d7024e.NewNetwork(me,*rt)
+	} else {
+		net = d7024e.NetworkJoin(&me)
+	}
+
+	kad := d7024e.NewKademlia(&net,20,3)
+	//completion := make(chan struct{})
+	go kad.Listen(ip/*,completion*/)
+	//kad.Network.SendFindContactMessage(&me)	
+
 	kad.Cli(os.Stdin)
+
+	//<-completion
 }
